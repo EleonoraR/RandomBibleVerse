fc5a4c737658f53e235c45f82d6b18a4ac6ee130
Ella R <61467087+EleonoraR@users.noreply.github.com>
2023-05-07 22:07:31 -0400

    Html, css and js uploaded
    Bible API - random verse

---
+++ script.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('.btn-verse');
+
+const motivation = function () {
+  alert('works');
+};
+btn.addEventListener('click', logJSONData);
+logJSONData();
+
+async function logJSONData() {
+  const response = await fetch(
+    'https://labs.bible.org/api/?passage=random&type=json'
+  );
+  const jsonData = await response.json();
+
+  document.querySelector('.book').textContent = `Book ${jsonData[0].bookname}`;
+  document.querySelector(
+    '.chapter'
+  ).textContent = `Chapter: ${jsonData[0].chapter}`;
+  document.querySelector('.text').textContent = `${jsonData[0].text}`;
+  document.querySelector('.verse').textContent = `Verse: ${jsonData[0].verse}`;
+  document.querySelector('.text').textContent = `${jsonData[0].text}`;
+
+  console.log(`Book ${jsonData[0].bookname}`);
+  console.log(`Chapter: ${jsonData[0].chapter}, Verse: ${jsonData[0].verse}`);
+  console.log(jsonData[0].text);
+}
